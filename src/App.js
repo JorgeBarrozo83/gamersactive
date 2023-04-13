0b273565559126713d282327e5f0c89ba0cc4ad2
JorgeBarrozo83 <jorgeluisbarrozo@hotmail.com.ar>
2023-04-13 20:50:17 -0300

    resumen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavBar from "../Componentes/NavBar/NavBar";
+import ItemListContainer from './Componentes/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
+      <ItemListContainer greeting={"Gamers Active"}/>
      <NavBar/>
     </div>
   );
